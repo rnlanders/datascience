26abb344815636b34c762917db912e26c0044859
Richard Landers <rlanders@tntlab.org>
2024-02-12 13:20:15 -0600

    Initial commit, just started week4

---
+++ projects/week4/R/week4.R
@@ -0,0 +1,7 @@
+# Script Settings and Resources
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(tidyverse)
+
+# Data Import
+import_tbl <- read_delim("../data/week4.dat", delim="-", col_names=c("casenum","parnum","stimver","datadate","qs"))
+glimpse(import_tbl)
