fe333e9b7d5b50376d1f194e651092728a6def69
rnlanders <rlanders@tntlab.org>
2026-01-26 14:36:15 -0600

    week 1 demo finalizing

--- R/week1_debrief.R
+++ R/week1_debrief.R
@@ -1,2 +1,13 @@
 # this is a new comment
 # this is also new
+
+library(tidyverse)
+library(lavaan)
+library(MASS)
+library(forcats)
+library(htmltools)
+library(rvest)
+
+?select
+??select
+find("select")
